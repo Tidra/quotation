e7892acaee5f8f61d65892651628522b4987f206
Tidra <349382581@qq.com>
2020-03-01 01:50:30 +0800

    1.0.1

--- miniprogram/pages/details/details.js
+++ miniprogram/pages/details/details.js
@@ -6,7 +6,37 @@ Page({
    */
   data: {
     is_hide: 'none',
+    select_id: 'mh',
+    /**
+     * code:代码
+     * name:名称
+     * current:当前价
+     * pre_close:昨收价
+     * change_pct:涨幅 (当前-昨收)/昨收
+     * change:涨跌 (当前-昨收)
+     * open:今开盘
+     * volume:成交量
+     * turnover_ratio:换手率
+     * high:最高
+     * low:最低
+     * sum_amount:成交额 (买入成交额+卖出成交额)
+     * sell:内盘
+     * buy:外盘
+     * market_cap:总市值(亿元)
+     * pe:市盈率
+     * amplitude:振幅 （当日最高点的价格－当日最低点的价格）/前收价
+     * circulating_market_cap:流通市值
+     */
+    value: {}
   },
+
+  // 选择显示图表
+  select: function (e) {
+    this.setData({
+      select_id: e.currentTarget.id
+    })
+  },
+
 
   // 显示搜索框
   seachIs: function() {
@@ -20,6 +50,7 @@ Page({
   },
 
   seach: function(e) {
+    this.seachIs();
     wx.navigateTo({
       url: '/pages/seach/seach?seach_value=' + e.detail.value.seach_value,
     })
@@ -29,7 +60,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    var value = {
+      code: options.code,
+      name: '中关村',
+      current: 15.85,
+      pre_close: 14.41,
+      change_pct: 99.9,
+      change: 1.44,
+      open: 14.25,
+      volume: 49.23,
+      turnover_ratio: 9.77,
+      high: 15.85,
+      low: 14.05,
+      sum_amount: 7.44,
+      sell: 23.85,
+      buy: 25.58,
+      market_cap: 103,
+      pe: 777.85,
+      amplitude: 12.49,
+      circulating_market_cap: 80.3
+    };
+    this.setData({
+      value: value
+    })
   },
 
   /**
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,6 +7,92 @@ Page({
    */
   data: {
     is_hide: 'none',
+    select_id: 'hushen',
+    /**
+     * code:代码
+     * name:名称
+     * current:当前价
+     * pre_close:昨收价
+     * change_pct:涨幅 (当前-昨收)/昨收
+     * change:涨跌 (当前-昨收)
+     */
+    value_index: {
+      hushen: [{
+        code: '000001',
+        name: '上证指数',
+        current: 2880.30,
+        pre_close: 2991.33,
+        change_pct: -3.71,
+        change: -111.03
+      }, {
+        code: '000906',
+        name: '中证800',
+        current: 4189.31,
+        pre_close: 4363.01,
+        change_pct: -3.98,
+        change: -173.70
+      }, {
+        code: '399004',
+        name: '深证100R',
+        current: 6149.33,
+        pre_close: 6412.27,
+        change_pct: -4.10,
+        change: -262.95
+      }, {
+        code: '399005',
+        name: '中小板指',
+        current: 7256.44,
+        pre_close: 7597.13,
+        change_pct: -4.48,
+        change: -340.69
+      }],
+      concepts: [],
+      hongkong: [],
+      globle: [],
+      others: []
+    },
+    value: {
+      hushen: [{
+        code: '688026',
+        name: '洁特生物',
+        current: 83.15,
+        pre_close: 69.30,
+        change_pct: 19.99,
+        change: 13.85
+      }, {
+          code: '688396',
+          name: '华润微',
+          current: 47.80,
+          pre_close: 42.01,
+          change_pct: 13.78,
+          change: 5.79
+        }, {
+          code: '688298',
+          name: '东方生物',
+          current: 102.23,
+          pre_close: 92.05,
+          change_pct: 11.06,
+          change: 10.18
+        }, {
+          code: '399005',
+          name: '中小板指',
+          current: 7256.44,
+          pre_close: 7597.13,
+          change_pct: -4.48,
+          change: -340.69
+        }],
+      concepts: [],
+      hongkong: [],
+      globle: [],
+      others: []
+    }
+  },
+
+  // 选择显示
+  select:function(e){
+    this.setData({
+      select_id: e.currentTarget.id
+    })
   },
 
   // 显示搜索框
@@ -21,6 +107,7 @@ Page({
   },
 
   seach: function(e) {
+    this.seachIs();
     wx.navigateTo({
       url: '/pages/seach/seach?seach_value=' + e.detail.value.seach_value,
     })
