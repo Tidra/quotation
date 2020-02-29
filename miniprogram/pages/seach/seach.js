3bf9d893553aa2e885450852a90f94788a51ad40
Tidra <349382581@qq.com>
2020-02-29 14:52:21 +0800

    1.0.0

---
+++ miniprogram/pages/seach/seach.js
@@ -0,0 +1,87 @@
+// miniprogram/pages/seach/seach.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    is_hide: 'none',
+  },
+
+  // 显示搜索框
+  seachIs: function() {
+    var is_hide = 'none';
+    if (this.data.is_hide == 'none') {
+      is_hide = 'flex';
+    }
+    this.setData({
+      is_hide: is_hide,
+    })
+  },
+
+  seach: function(e) {
+    this.setData({
+      seach_value: e.detail.value.seach_value
+    });
+    this.seachIs();
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log(options);
+    this.setData({
+      seach_value: options.seach_value,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
